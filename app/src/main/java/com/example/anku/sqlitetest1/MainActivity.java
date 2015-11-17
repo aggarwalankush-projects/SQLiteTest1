0eae41bffb34ec9aee0429b74816104299e27b8a
Ankush Aggarwal <ankushagg93@gmail.com>
2015-11-16 23:36:25 -0500

    basic create insertion from MainActivity

--- app/src/main/java/com/example/anku/sqlitetest1/MainActivity.java
+++ app/src/main/java/com/example/anku/sqlitetest1/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.anku.sqlitetest1;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +26,33 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+
+
+        SQLiteDatabase sqLiteDatabase = getBaseContext().openOrCreateDatabase("sqlite-test-1.db", MODE_PRIVATE, null);
+        final String DROP_TABLE="DROP TABLE IF EXISTS contacts";
+        final String CREATE_TABLE="CREATE TABLE contacts(name TEXT, phone INTEGER, email TEXt)";
+        final String insert1="INSERT INTO contacts VALUES('ankush', 656564, 'ankush@gmail.com')";
+        final String insert2="INSERT INTO contacts VALUES('ankit', 374746, 'ankit@gmail.com')";
+        final String query = "Select * from contacts";
+        sqLiteDatabase.execSQL(DROP_TABLE);
+        sqLiteDatabase.execSQL(CREATE_TABLE);
+        sqLiteDatabase.execSQL(insert1);
+        sqLiteDatabase.execSQL(insert2);
+        Cursor cursor = sqLiteDatabase.rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                String name = cursor.getString(0);
+                int phone = cursor.getInt(1);
+                String email = cursor.getString(2);
+                Toast.makeText(getBaseContext(), name + " - " + phone + " - " + email, Toast.LENGTH_LONG).show();
+            } while (cursor.moveToNext());
+        }else {
+            Toast.makeText(getBaseContext(), "Error retrieving data", Toast.LENGTH_LONG).show();
+        }
+        cursor.close();
+        sqLiteDatabase.close();
+
     }
 
     @Override
